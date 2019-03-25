8b594ffabf992f149580ee8a28b8dc4c057acab5
Jo Seoyeon <joseoyeon60@gmail.com>
2019-03-25 11:17:47 +0900

    1-24 commet, image, like

--- nomadgram/images/admin.py
+++ nomadgram/images/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Image) #d
+class ImageAdmin(admin.ModelAdmin):
+    pass #null class
+
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Comment)
+class CommantAdmin(admin.ModelAdmin):
+    pass
