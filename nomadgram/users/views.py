9d3954d8dbed72383ddeef256cb8d23dc81501b1
Jo Seoyeon <joseoyeon60@gmail.com>
2019-05-12 23:36:25 +0900

    #1-67 Update Profile

--- nomadgram/users/views.py
+++ nomadgram/users/views.py
@@ -36,16 +36,42 @@ class UnFollowUser(APIView):
         return Response(status =200)
 
 class UserProfile(APIView) :
+    def get_user(self, username):
+        try: 
+            found_user =models.User.objects.get(username=username)
+            return found_user
+        except models.User.DoesNotExist:
+            return None
+
     def get(self, request, username, format=None):
         
-        try :
-            found_user = models.User.objects.get(username=username)
-        except models.User.DoesNotExist:
-            return Response(status  =404)
+        found_user = self.get_user(username)
         
+        if found_user is None : 
+            return Response(status =status.HTTP_404_NOT_FOUND)
+
         serializer = serializers.UserProfileSerializer(found_user)
 
         return Response(data=serializer.data, status=200)
+    
+    def put(self, request, username, format=None):
+        user = request.user
+        found_user = self.get_user(username)
+        
+        if found_user is None : 
+            return Response(status =status.HTTP_404_NOT_FOUND)
+            
+        elif found_user.username != user.username :
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        else :
+            serializer = serializers.UserProfileSerializer(found_user, data=request.data, partial=True)
+
+            if serializer.is_valid() :
+                serializer.save()
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+            else : 
+                return Response(data=serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 class UserFollowers(APIView):
     def get(self, request, username, format=None) : 
