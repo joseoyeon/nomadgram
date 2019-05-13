bdea4886858b3694ab82ba4fca949674dc148b02
Jo Seoyeon <joseoyeon60@gmail.com>
2019-05-14 02:03:49 +0900

    #1-69 What is JWT

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("<username>/following/", view=views.UserFollowing.as_view(), name="user_following"),
     path("search/", view=views.Search.as_view(), name="user_search"),
     path("<username>/", view=views.UserProfile.as_view(), name="user_profile"),
+    path("<username>/password/", view=views.ChangePassword.as_view(), name="change"),
 ]
 
 '''
--- nomadgram/users/views.py
+++ nomadgram/users/views.py
@@ -108,6 +108,29 @@ class Search(APIView) :
         else : 
             return Response( status =404)
 
+class ChangePassword(APIView) :
+    def put(self, request, username, format = None)  :
+        user = username
+        if user.username == username :
+            current_password = request.data.get('current_password', None)
+            if current_password is not None :
+                passwords_match = user.check_password(current_password)
+                if password_match : 
+                    new_password = request.data.get('new_password', None)
+                    if new_password  is not None :
+                        user.set_password(new_password)
+                        user.save()
+                        return Response(status = status.HTTP_200_OK)
+                    else : 
+                        return Response(status = status.HTTP_400_BAD_REQUEST)
+                else :
+                    return Response(status = status.HTTP_400_BAD_REQUEST)
+            else :
+                return Response(status = status.HTTP_400_BAD_REQUEST)
+        else :
+            return Response(status= status.HTTP_400_BAD_REQUEST)
+
+
 """ def UserFollowingFBC(request, username) : 
     if request.method == 'GET':
         '''request.POST.get('key', value)
